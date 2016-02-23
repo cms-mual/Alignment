aa63695e605d085a31370ae52a991652b50e8543
Aysen Tatarinov <aysen.tatarinov@cern.ch>
2016-02-23 20:45:09 +0100

    sync with CMSSW_8_0_0_patch1

---
+++ MuonAlignmentAlgorithms/interface/MuonCSCChamberResidual.h
@@ -0,0 +1,28 @@
+#ifndef Alignment_MuonAlignmentAlgorithms_MuonCSCChamberResidual_H
+#define Alignment_MuonAlignmentAlgorithms_MuonCSCChamberResidual_H
+
+/** \class MuonCSCChamberResidual
+ * 
+ * Implementation of muon chamber residuals for CSC
+ * 
+ * $Id: $
+ */
+
+#include "Alignment/MuonAlignmentAlgorithms/interface/MuonHitsChamberResidual.h"
+#include "TrackingTools/GeomPropagators/interface/Propagator.h"
+
+class MuonCSCChamberResidual: public MuonHitsChamberResidual
+{
+public:
+  MuonCSCChamberResidual(edm::ESHandle<GlobalTrackingGeometry> globalGeometry, AlignableNavigator *navigator,
+                         DetId chamberId, AlignableDetOrUnitPtr chamberAlignable);
+
+  // for CSC, the residual is chamber local x, projected by the strip measurement direction
+  // for CSC, the resslope is dresx/dz, or tan(phi_y)
+  virtual void addResidual(edm::ESHandle<Propagator> prop, const TrajectoryStateOnSurface *tsos, const TrackingRecHit *hit,double, double);
+
+  // dummy method
+  virtual void setSegmentResidual(const reco::MuonChamberMatch *, const reco::MuonSegmentMatch *) {}
+};
+
+#endif // Alignment_MuonAlignmentAlgorithms_MuonCSCChamberResidual_H
