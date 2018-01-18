1b5a6770a2470b0a071213c832d74d472520fbea
Luca Pernie <mrluca87@gmail.com>
2018-01-18 14:57:09 -0600

    Revert "Cmssw 10 0 0 pre2"

--- MuonAlignmentAlgorithms/interface/MuonResiduals5DOFFitter.h
+++ MuonAlignmentAlgorithms/interface/MuonResiduals5DOFFitter.h
@@ -53,7 +53,7 @@ public:
   };
 
   MuonResiduals5DOFFitter(int residualsModel, int minHits, int useResiduals, bool weightAlignment=true): MuonResidualsFitter(residualsModel, minHits, useResiduals, weightAlignment) {}
-  ~MuonResiduals5DOFFitter() override{}
+  ~MuonResiduals5DOFFitter(){}
 
   int type() const override { return MuonResidualsFitter::k5DOF; }
 
