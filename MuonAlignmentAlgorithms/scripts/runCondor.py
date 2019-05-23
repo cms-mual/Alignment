2febd7ae2a7a1e5d7924cf7204f2c2eac906d66d
hyunyong <antilogy33@gmail.com>
2019-05-23 09:17:45 -0500

    Create runCondor.py
    HTCondor submit
    Usage: make a batch job dir with createJobs.py then 
    python  runCondor.py -d outPutDirOfCreateJobs

---
+++ MuonAlignmentAlgorithms/scripts/runCondor.py
@@ -0,0 +1,87 @@
+import os, sys, optparse
+
+usage = "-d script DIR -e excute -n batchName"
+parser = optparse.OptionParser(usage)
+parser.add_option("-d", "--tdir",
+                   help="shell script dir",
+                   type="string",
+                   dest="tdir")
+parser.add_option("-e", "--excute",
+                   help="excute condor submit",
+                   default=True,
+                   dest="excute")
+parser.add_option("-n", "--name",
+                   help="bach name",
+                   type="string",
+                   default="muonAlignment",
+                   dest="batchName")
+
+options, args = parser.parse_args(sys.argv[1:])
+print "target DIR:",options.tdir
+
+cDir = options.tdir+"_condor"
+os.mkdir(cDir)
+
+homePath = "/".join(os.environ['CMSSW_BASE'].split("/")[:-1])
+cmsswName = os.environ['CMSSW_BASE'].split("/")[-1]
+os.chdir(homePath)
+os.system("tar cf "+cmsswName+".tar "+cmsswName)
+tarball = cmsswName+".tar"
+os.system("mv "+tarball+" "+cmsswName+"/src/"+cDir)
+os.chdir(cmsswName+"/src/")
+
+sList = [x for x in os.listdir(options.tdir) if x.startswith("gather") and x.endswith(".sh")]
+#print sList
+
+cTmp = """executable              = {sh}
+universe                = vanilla
+requirements            = (OpSysAndVer =?= "CentOS7")
++JobFlavour             = "tomorrow"
+output                  = {shN}.out
+error                   = {shN}.err
+log                     = {shN}.log
+transfer_input_files    = {tarBall}
+should_transfer_files   = YES
+when_to_transfer_output = ON_EXIT
+transfer_output_files   = {outN}
+queue
+"""
+
+cpJson = """if [ "zzz$ALIGNMENT_JSON" != "zzz" ]; then
+  cp -f $ALIGNMENT_JSON $ALIGNMENT_CAFDIR/
+fi
+"""
+
+for x in sList:
+  tmpJ = open(cDir+"/"+x.replace(".sh", ".sub"),'w')
+  data = {'sh':x, 'shN':x.replace(".sh", ""), 'tarBall':tarball, 'outN':'plotting'+x[6:9]+".root"}
+  tmpJ.write(cTmp.format(**data))
+  tmpJ.close()
+
+  tmpS = open(cDir+"/"+x, 'w')
+  tmpS.write("#!/bin/bash\n")
+  tmp = open(options.tdir+"/"+x, 'r')
+  for x in tmp:
+    if x.startswith("export ALIGNMENT_CAFDIR=`pwd`\n"):
+      tmpS.write(x)
+      tmpS.write("tar xf "+tarball)
+      tmpS.write("\ncd "+tarball.replace(".tar","/src"))
+      tmpS.write("\nscram build ProjectRename")
+      tmpS.write("\neval `scramv1 run -sh`\n")
+    elif x.startswith("export"): tmpS.write(x)
+    elif x.startswith("cp -f"):
+      tmpS.write(x)
+      tmpS.write(cpJson)
+      tmpS.write("cd $ALIGNMENT_CAFDIR/\n")
+      tmpS.write("cmsRun gather_cfg.py\n")
+      break
+  tmpS.close()
+
+os.chdir(cDir)
+cList = [x for x in os.listdir(".") if x.endswith(".sub")]
+for i, x in enumerate(cList):
+  os.system("chmod 755 "+x.replace(".sh", ".sub"))
+  os.system("chmod 755 "+x)
+  if options.excute:
+    print "condor submit {}/{}".format(i,len(cList))
+    os.system("condor_submit -batch-name "+options.batchName+" "+x.replace(".sh", ".sub"))
