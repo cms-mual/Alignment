33831b7661fccc043201b9c6a84572963c1f9b61
hyunyong <antilogy33@gmail.com>
2019-12-09 12:35:56 -0600

    Update runCondor2.py

--- MuonAlignmentAlgorithms/scripts/runCondor2.py
+++ MuonAlignmentAlgorithms/scripts/runCondor2.py
@@ -14,7 +14,7 @@ def writeSub(arg, fname):
   queue
   """
   tmp = open(fname,'w')
-  tmw.write(cTmp.format(**arg))
+  tmp.write(cTmp.format(**arg))
   tmp.close()
 
 usage = "-d script DIR -t testMode -n batchName"
@@ -32,7 +32,7 @@ parser.add_option("-n", "--name",
 options, args = parser.parse_args(sys.argv[1:])
 cmsswdir = os.getcwd()
 if not cmsswdir.startswith("/afs/cern.ch/work/"):
-  print "runCondor2.py should be running in /afs/cern.ch/work/"
+  print "runCondor2.py should be running on /afs/cern.ch/work/"
   exit()
 
 if options.tdir.endswith("/"): tdir = options.tdir[:-1]
@@ -40,28 +40,28 @@ else: tdir = options.tdir
 print "target DIR:",tdir
 cdir = options.tdir+"_condor"
 os.mkdir(cdir)
-
+savedir = cmsswdir+"/"+tdir+"/"
 # gather part
 gList = [x for x in os.listdir(tdir) if x.startswith("gather") and x.endswith(".sh")]
 runGather = open(cdir+"/runGather.sh",'w')
+runGather.write("#!/bin/sh\n")
 for x in gList:
-  tmpArg = {'sh':cmsswdir+"/"+cdir+"/"+x, 'shN':x.replace(".sh",""), 'nCPU':1, 'memory':'4000'}
+  tmpArg = {'sh':savedir+x, 'shN':x.replace(".sh",""), 'nCPU':1, 'memory':'4000'}
   fname = x.replace(".sh", ".sub")
   writeSub(tmpArg, cdir+"/"+fname)
-  runGather.write("condor_submit -batch-name align_"+options.batchName+" "+fname)
-
+  runGather.write("condor_submit -batch-name align_"+options.batchName+" "+fname+"\n")
+os.system("chmod +x "+cdir+"/runGather.sh")
 # align part
 runAlign = open(cdir+"/runAlign.sh", 'w')
-tmpArg = {'sh':cmsswdir+"/"+cdir+'/align.sh', 'shN':'align', 'nCPU':4, 'memory':'4000'}
+tmpArg = {'sh':savedir+'align.sh', 'shN':'align', 'nCPU':4, 'memory':'4000'}
 fname = "align.sub"
 writeSub(tmpArg, cdir+"/"+fname)
-runAlign.write("condor_submit -batch-name "+options.batchName+" "+fname)
-
+runAlign.write("#!/bin/sh\ncondor_submit -batch-name align_"+options.batchName+" "+fname)
+os.system("chmod +x "+cdir+"/runAlign.sh")
 # validation part
 runValidation = open(cdir+"/runValidation.sh", 'w')
-tmpArg = {'sh':cmsswdir+"/"+cdir+'/validation.sh', 'shN':'validation', 'nCPU':4, 'memory':'4000'}
+tmpArg = {'sh':savedir+'validation.sh', 'shN':'validation', 'nCPU':4, 'memory':'4000'}
 fname = "validation.sub"
 writeSub(tmpArg, cdir+"/"+fname)
-runValidation.write("condor_submit -batch-name validation_"+options.batchName+" "+fname)
-
-
+runValidation.write("#!/bin/sh\ncondor_submit -batch-name validation_"+options.batchName+" "+fname)
+os.system("chmod +x "+cdir+"/runValidation.sh")
